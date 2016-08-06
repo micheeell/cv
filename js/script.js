6b91f794082409fd2f30502ddbfe9641389221a3
Michael Ayoul <mayoul@lyracons.com>
2016-08-06 02:14:22 -0300

    first commit - CV en español

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(function() {
+    // Tipsy
+    $('.north').tipsy({gravity: 'n'});
+});
+
+$(document).ready(function() {
+    // Contact Form Box
+    $("#contact").colorbox({width:"500px", height:"640px", iframe:true});
+});
